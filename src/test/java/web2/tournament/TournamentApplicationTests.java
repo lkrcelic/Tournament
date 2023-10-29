1ce7097b8ae8bc60fc8f296f96ea1e7916638b87
lkrcelic <lkrcelic@croz.net>
2023-10-29 13:47:05 +0100

    Initial commit

---
+++ src/test/java/web2/tournament/TournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package web2.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
